2bbc9cadfb478d7648596f6c59523ac56ce0afb3
Daniel <a27314@svalero.com>
2024-11-06 12:23:46 +0100

    Subida de ejercicio 10 de la UD1

---
+++ UNIDAD1/10_Seconds_Converter.js
@@ -0,0 +1,16 @@
+const assert = require('assert').strict;
+
+function toHoursMinutesSeconds(value) {
+    const hours = Math.floor(value / 3600);
+    const minutes = Math.floor((value % 3600) / 60);
+    const seconds = value % 60;
+    
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+// Pruebas
+assert.deepStrictEqual(toHoursMinutesSeconds(3600), "1:0:0");
+assert.deepStrictEqual(toHoursMinutesSeconds(3720), "1:20:0");
+assert.deepStrictEqual(toHoursMinutesSeconds(3725), "1:20:5");
+
+console.log("Todo bein.");
