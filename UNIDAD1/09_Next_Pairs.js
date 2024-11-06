1b5aafe4f6fa6c38739beecdfd21b3f1b378682a
Daniel <a27314@svalero.com>
2024-11-06 12:21:51 +0100

    Subida de ejercicio 09 de la UD1

---
+++ UNIDAD1/09_Next_Pairs.js
@@ -0,0 +1,17 @@
+const assert = require('assert').strict;
+
+function nextPairs(value) {
+    if (value % 2 === 0) {
+        
+        return [value - 2, value + 2];
+    } else {
+        
+        return [value - 1, value + 1];
+    }
+}
+
+// Pruebas
+assert.deepStrictEqual(nextPairs(3), [2, 4]);
+assert.deepStrictEqual(nextPairs(4), [2, 6]);
+
+console.log("Todo Corectou");
